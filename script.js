930ec4ebd6e9c359fe905093b4e9ed6d69d6cc12
benjamin »beni« sahin <57801294+beniwonka@users.noreply.github.com>
2020-03-14 15:30:52 +0100

    filter gallery added

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+$(document).ready(function(){
+	$(".buttonGraph").click(function(){
+		var value = $(this).attr("data-filter");
+		if (value == "allGraph")
+		{
+			$(".filtergraph").slideDown("slow");
+		}
+		else 
+		{
+			$(".filtergraph").not("."+value).slideDown("slow");
+			$(".filtergraph").filter("."+value).slideUp("slow");
+		}
+
+		$("div .buttonGraph").click(function(){
+			$(this).addClass('activeGraph').siblings().removeClass('activeGraph');
+		})
+
+	})
+
+	$(".buttonPhoto").click(function(){
+		var value = $(this).attr("data-filter");
+		if (value == "allPhoto")
+		{
+			$(".filterphoto").slideDown("slow");
+		}
+		else 
+		{
+			$(".filterphoto").not("."+value).slideDown("slow");
+			$(".filterphoto").filter("."+value).slideUp("slow");
+		}
+
+		$("div .buttonPhoto").click(function(){
+			$(this).addClass('activePhoto').siblings().removeClass('activePhoto');
+		})
+
+})
+
+})
