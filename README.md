bf399c6c712ee265245a783b100a4f1ea613b18c
beni wonka <57801294+beniwonka@users.noreply.github.com>
2024-01-14 21:15:12 +0100

    WEB UPDATE 2024

--- README.md
+++
@@ -1,4 +0,0 @@
-# BENI WONKA WEBSITE
-
-Building my first own website. 
-[how it looks right now](https://beniwonka.github.io/beniwonka-website/)
--- script.js
+++ script.js
@@ -1,41 +1,71 @@
+function openAbout() {
+  var ab = document.getElementById("pageAbout");
+  if (ab.style.visibility === "hidden") {
+    ab.style.visibility = "unset";
+  } else {
+    ab.style.visibility = "hidden";
+  }
+}
 
+function openGe() {
+  var a = document.getElementById("page1");
+  if (a.style.transform === "translate(100vh, 100vw)") {
+    a.style.transform = "translate(0vh, 0vw)";
+  } else {
+    a.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
+function openKo() {
+  var b = document.getElementById("page2");
+  if (b.style.transform === "translate(100vh, 100vw)") {
+    b.style.transform = "translate(0vh, 0vw)";
+  } else {
+    b.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
-$(document).ready(function(){
-	$(".buttonGraph").click(function(){
-		var value = $(this).attr("data-filter");
-		if (value == "allGraph")
-		{
-			$(".filtergraph").slideDown("slow");
-		}
-		else 
-		{
-			$(".filtergraph").not("."+value).slideDown("slow");
-			$(".filtergraph").filter("."+value).slideUp("slow");
-		}
+function openAa() {
+  var c = document.getElementById("page3");
+  if (c.style.transform === "translate(100vh, 100vw)") {
+    c.style.transform = "translate(0vh, 0vw)";
+  } else {
+    c.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
-		$("div .buttonGraph").click(function(){
-			$(this).addClass('activeGraph').siblings().removeClass('activeGraph');
-		})
+function openNeo() {
+  var d = document.getElementById("page4");
+  if (d.style.transform === "translate(100vh, 100vw)") {
+    d.style.transform = "translate(0vh, 0vw)";
+  } else {
+    d.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
-	})
+function openCov() {
+  var e = document.getElementById("page5");
+  if (e.style.transform === "translate(100vh, 100vw)") {
+    e.style.transform = "translate(0vh, 0vw)";
+  } else {
+    e.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
-	$(".buttonPhoto").click(function(){
-		var value = $(this).attr("data-filter");
-		if (value == "allPhoto")
-		{
-			$(".filterphoto").slideDown("slow");
-		}
-		else 
-		{
-			$(".filterphoto").not("."+value).slideDown("slow");
-			$(".filterphoto").filter("."+value).slideUp("slow");
-		}
+function openPost() {
+  var f = document.getElementById("page6");
+  if (f.style.transform === "translate(100vh, 100vw)") {
+    f.style.transform = "translate(0vh, 0vw)";
+  } else {
+    f.style.transform = "translate(100vh, 100vw)";
+  }
+}
 
-		$("div .buttonPhoto").click(function(){
-			$(this).addClass('activePhoto').siblings().removeClass('activePhoto');
-		})
-
-})
-
-})
+function openCap() {
+  var f = document.getElementById("page7");
+  if (f.style.transform === "translate(100vh, 100vw)") {
+    f.style.transform = "translate(0vh, 0vw)";
+  } else {
+    f.style.transform = "translate(100vh, 100vw)";
+  }
+}
